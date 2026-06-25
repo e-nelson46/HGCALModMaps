fd395a631a3b30bf60a618bfa88615feee8094e6
Ethan Nelson <ethan@cmslpc365.fnal.gov>
2026-06-25 09:02:38 -0500

    Adding folder for test maps

--- cassette_plotter.py
+++ cassette_plotter.py
@@ -32,10 +32,43 @@ df=pd.read_csv('geometry_sipmontile_v16.6.hgcal.txt',delim_whitespace=True)
 doc = ezdxf.new("R2010")
 msp = doc.modelspace()
 
+############Cutting panda dataframe############
+
+
+df = df[(df.plane == 33) & (df.icassette == 2)]
+
+col = [
+    'plane',
+    'u',
+    'v',
+    'itype',
+    #'typecode',
+    'x0',
+    'y0',
+    'irot',
+    'nvertices',
+    'vx_0','vy_0','vx_1','vy_1','vx_2','vy_2','vx_3','vy_3','vx_4','vy_4','vx_5','vy_5','vx_6','vy_6',
+    'isEngine'
+]
+
+cass = df[col]
+print("Cassestte dataframe:")
+print(cass)
+############Drawing the objects############
+print("Drawing modules...")
+for module in cass:
+    origin_x, origin_y = module.loc('x0'), module.loc('y0')
+    print(f"Finished module {module}")
+
+
 ############Saving objects to file#############
 filename = "Test_0_1"
-output_folder = r"TestDXFfiles"
+output_folder = "TestDXFfiles"
 hexagon_path = os.path.join(output_folder, filename)
 
-doc.saveas(hexagon_path)
+#doc.saveas(hexagon_path)
 print("DXF file successfully created!")
+
+
+
+
